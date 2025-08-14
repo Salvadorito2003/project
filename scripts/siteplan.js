5024387f089adb865b2588da66981fc04923ce7f
Tobias <tobiasaguilartarea@gmail.com>
2025-08-14 19:06:36 -0300

    first commit

---
+++ scripts/siteplan.js
@@ -0,0 +1,13 @@
+const yearElement = document.querySelector("#currentyear");
+const currentYear = new Date().getFullYear();
+yearElement.textContent = `© ${currentYear}`;
+
+const menuButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation")
+menuButton.addEventListener("click", () => {
+	navigation.classList.toggle("open");
+	menuButton.classList.toggle("open");
+});
+
+const lastModifiedElement = document.querySelector("#lastmodified");
+lastModifiedElement.textContent = `Last Modified: ${document.lastModified}`;
